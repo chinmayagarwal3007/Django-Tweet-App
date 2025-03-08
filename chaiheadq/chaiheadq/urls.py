c868c0f43222514eaf38b75863e906e340b33f21
Chinmay <agarwalchinmay3007@gmail.com>
2025-03-08 17:13:24 +0530

    done

--- chaiheadq/chaiheadq/urls.py
+++ chaiheadq/chaiheadq/urls.py
@@ -3,10 +3,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from django.contrib.auth.urls import views as auth_views
 
 
 urlpatterns = [
     path("", views.home, name="home"),
     path('admin/', admin.site.urls),
-    path('tweet/',include('tweet.urls'))
+    path('tweet/',include('tweet.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- chaiheadq/tweet/forms.py
+++ chaiheadq/tweet/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Tweet
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class TweetForm(forms.ModelForm):
@@ -10,3 +12,9 @@ class TweetForm(forms.ModelForm):
             'text': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter your tweet here'}),
             'photo': forms.FileInput(attrs={'class': 'form-control-file'}),
         }
+    
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
